4b371f81ff4b09e281fdba4944d49600eba1cea8
坤 陈 <1010453408@qq.com>
2022-07-18 11:32:33 +0800

    新建了链接数据库帮助类

---
+++ Common/Helper/SqlDBHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Common.Helper
+{
+    /// <summary>
+    /// 数据库访问类
+    /// </summary>
+    public class SqlDBHelper
+    {
+        //数据库链接串
+        private static string connString = ConfigurationManager.ConnectionStrings["Books"].ToString();
+
+        /// <summary>
+        /// 打开数据库链接
+        /// </summary>
+        /// <param name="sql">sql语句</param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static SqlConnection ExecuteNonQuery(string sql,params SqlParameter[] parameters)
+        {
+            SqlConnection conn = new SqlConnection(connString);
+;           conn.Open();
+
+            return conn;
+        }
+
+    }
+}
